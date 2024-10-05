5f909c638708eb5d4bed49f8b85530d89a6db47e
Kondrov220 <121194027+Kondrov220@users.noreply.github.com>
2024-10-05 17:08:38 +0300

    Add files via upload

---
+++ string.js
@@ -0,0 +1,22 @@
+//1
+const result = 5 + 5 + "5";
+console.log(typeof result);
+
+//2
+const email = "danay.k.24.12@gmail"
+const text = email.includes("@");
+const textTwo = email.length;
+console.log(text , textTwo);
+
+//3
+const my = "my";
+const names = "name";
+const is = "is";
+let fullName = my + " " + names + " " + is;
+fullName += " Viktor"
+console.log(fullName);
+
+//4
+const userName = "Олександро";
+const payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
